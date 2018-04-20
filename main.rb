224ba4964c2be38f5e204d82304c28ca5c226402
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-04-20 04:27:37 +0000

    Add view from input variable

--- main.rb
+++ main.rb
@@ -1,9 +1,10 @@
 require './system'
 
 static void main(ARGV){
-    scan = new_ Scanner(System.in)
+    scan = new_ Scanner(System.in);
     System.out.printf('入力:');
-    line = scan.nextLine()
+    line = scan.nextLine();
     System.out.println('Hello, world!');
+    System.out.println(line);
 }
 
