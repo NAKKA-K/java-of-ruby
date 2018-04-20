d7dbdae3c2db8d407ef782c88b8f1320164a0a74
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-04-20 03:56:25 +0000

    Add system method

--- system.rb
+++ system.rb
@@ -13,14 +13,44 @@ class Out
     def println(str)
         puts str
     end
+
+    def printf(str)
+        print str
+    end
+end
+
+class In
 end
 
 class System
     @@out = Out.new
+    @@in = In.new
 
     class << self
         def out
             @@out
         end
+
+        def in
+            @@in
+        end
     end
+end
+
+def new_(class_obj)
+    Scan.new(class_obj)
+end
+
+class Scan
+    def initialize(fp)
+        @fp = fp
+    end
+
+    def nextLine
+        gets
+    end
+end
+
+def Scanner(obj)
+    obj
 end
